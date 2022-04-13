a3cd04e945ec056fd9608f26e70806dbeaeaa5d1
Erika de Freitas <erikafreitas47@gmail.com>
2022-04-13 13:05:00 -0300

    aulas 12 e 13 de abril

---
+++ aula1304/scripts/classes.js
@@ -0,0 +1,36 @@
+var urlBase = "http://localhost:3000/contatos";
+
+class Contato {
+    constructor (id, nome, fone, email) {
+        this.id = id,
+        this.nome = nome,
+        this.fone = fone,
+        this.email = email
+    }
+
+    salvar () {
+        console.log(this);
+        fetch(urlBase, {
+                method: 'POST',
+                body: JSON.stringify(this),
+                headers: {
+                    'Content-type' : 'Application/json'
+                }
+            }
+        )
+        .catch(erro => console.log(erro))         
+    }
+
+    consultarTodos (display) {
+        fetch(urlBase)
+            .then(x => x.text())
+            .then(data => display(data))
+    }
+
+    consultarById (idContato, display) {
+        fetch(`${urlBase}/${idContato}`)
+            .then(x => x.text())
+            .then(data => display(data))
+    }   
+
+}
